aacb386e96325ba30339293c2c544286fb86d02e
Pedro Adame Vergara <pedro.a.1smr@gmail.com>
2017-10-18 22:29:54 +0200

    Remove test controller and keep spec/features folder

--- spec/features/test_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.feature 'blah', type: :feature do
-  scenario 'well k psa', js: true do
-    visit "/pages/index"
-    expect(page).to have_text("Find me")
-  end
-end
