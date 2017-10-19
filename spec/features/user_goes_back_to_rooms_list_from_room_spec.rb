9ce4175034e9268a7bf62ab104b6ce8bd8ffefd9
Pedro Adame Vergara <pedro.a.1smr@gmail.com>
2017-10-19 18:43:57 +0200

    Go back to room list

---
+++ spec/features/user_goes_back_to_rooms_list_from_room_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+require 'support/login_helper'
+
+RSpec.feature "UserGoesBackToRoomsListFromRoom", type: :feature do
+  include LoginHelper
+
+  before do
+    login_with_capybara!
+  end
+
+  let!(:room) { create :room }
+
+  scenario "User clicks go back button and goes to room list" do
+    visit room_url(room)
+    click_link "Salir de la sala"
+    expect(page).to have_text("Elige una sala")
+    expect(page).to have_css(".rooms")
+  end
+end
