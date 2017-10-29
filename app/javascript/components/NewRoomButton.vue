9fd5ebe432eed6c170ffa7753cbe84862743e534
Pedro Adame Vergara <pedro.a.1smr@gmail.com>
2017-10-29 16:24:08 +0100

    Add new room button component

---
+++ app/javascript/components/NewRoomButton.vue
@@ -0,0 +1,16 @@
+<template lang='pug'>
+  a.level-item.button.is-link(@click.prevent="launchModal") Nueva sala
+</template>
+
+<script>
+export default {
+  methods: {
+    launchModal: function () {
+      console.log("illorl")
+    }
+  }
+}
+</script>
+
+<style>
+</style>
