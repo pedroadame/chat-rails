0e8f954bed3a119492e89ca52967979263c753cb
Pedro Adame Vergara <pedro.a.1smr@gmail.com>
2017-10-29 16:24:42 +0100

    Build room list

--- app/javascript/views/room_list.vue
+++ app/javascript/views/room_list.vue
@@ -1,3 +1,24 @@
-<template>
- <p>Lista de salas</p>
+<template lang='pug'>
+  section.section
+    .container
+      .level
+        .level-left
+        .level-right
+          new-room-button
+
+    .container
+      .rooms
 </template>
+
+<script>
+import NewRoomButton from '../components/NewRoomButton.vue'
+export default {
+  components: {
+    NewRoomButton
+  }
+}
+</script>
+
+<style lang='sass' scoped>
+
+</style>
