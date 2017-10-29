d194a87610f992685e5e037cd44532166939fbd1
Pedro Adame Vergara <pedro.a.1smr@gmail.com>
2017-10-29 22:31:15 +0100

    Remove user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  private
-  def authenticate_user!
-    # if current_user.nil?
-      # redirect_to root_path, notice: "Debes registrarte antes"
-    # end
-  end
-
-  def current_user
-    # @current_user ||= User.find_by_id(session[:user_id])
-  end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,4 @@
 class MessagesController < ApplicationController
-  before_action :authenticate_user!
   before_action :set_room
 
   def create
